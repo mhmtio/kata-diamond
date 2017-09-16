6ce199199c4d8f491405fa91b1050f5210a5d436
Matthias Hanitzsch-Moonlight <mhm@terrafino.io>
2017-09-16 14:04:49 +0100

    Amended implementation to create only the top left half of the diamond, then mirror that to add the top right half, then mirror again to add the bottom half.

--- src/main/scala/io/terrafino/kata/diamond/DiamondBuilder.scala
+++ src/main/scala/io/terrafino/kata/diamond/DiamondBuilder.scala
@@ -6,12 +6,11 @@ class DiamondBuilder() {
 
   def build(letter: Char): String = {
     val diff = letter - A
-    val lines = for {i <- 0 to diff
+    val topLeftHalf = for {i <- 0 to diff
                      buf1 = " " * (diff - i)
-                     buf2 = " " * (i*2-1)
-                     char = (A + i).toChar
-    } yield if (i == 0) s"$buf1$char$buf1" else s"$buf1$char$buf2$char$buf1"
-    (lines ++ lines.take(diff).reverse).mkString("", "\n", "\n")
+                     buf2 = " " * i
+    } yield s"$buf1${(A+i).toChar}$buf2"
+    val topHalf = topLeftHalf.map(l => l + l.reverse.tail)
+    (topHalf ++ topHalf.take(diff).reverse).mkString("", "\n", "\n")
   }
-
 }
